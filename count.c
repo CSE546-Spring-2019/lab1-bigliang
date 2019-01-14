785d23c85b8b84c8e4c6a10ac2a29987907dbcbb
bigliang <407142269@qq.com>
2019-01-14 15:10:04 -0500

    Add files via upload
    lab1

---
+++ count.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+
+    if ( argc < 4) {
+        printf("Missing parameters! Please provide three parameters");
+        exit(1);
+    }
+    if (argc > 4) {
+        printf("Too many parameters! Please provide three parameters");
+        exit(1);
+    }
+    FILE *inputFile;
+    FILE *outputFile;
+    if ((inputFile = fopen(argv[1],"rb")) == NULL) {
+        printf("Can't open file %s \n", argv[1]);
+        printf("The program will exit, please rerun it with correct input file name \n");
+        return 0;
+    }
+    if ((outputFile = fopen(argv[3],"wb+")) == NULL) {
+        printf("Can't open file %s \n", argv[3]);
+        printf("The program will exit, please rerun it with correct output file name \n");
+        return 0;
+    }
+
+    int len = (int)strlen(argv[2]);
+    char *searchString = malloc(len);
+    stpcpy(searchString,argv[2]);
+    if (len > 20) {
+        printf("The search string must less than 20 byte \n");
+        exit(1);
+    }
+    int size = 0;
+    int numbers = 0;
+    char *readString = malloc(100);
+    fseek (inputFile, 0, SEEK_SET);
+    int readSize = (int)fread(readString,1,100,inputFile);
+    size = readSize;
+    while (readSize >= len) {
+        for (int i = 0; i < readSize && readSize - i >= len; i++) {
+            if (readString[i] == (searchString[0] & 0xff)) {
+                int matched = 1;
+                for (int j = 0; j < len; j++) {
+                    if (readString[i+j] != (searchString[j]& 0xff) ) {
+                        matched = 0;
+                        break;
+                    }
+                }
+                if (matched == 1) numbers++;
+            }
+        }
+        if (readSize < 100) break;
+        int moved = -len + 1;
+        fseek(inputFile,moved,SEEK_CUR);
+        readSize = (int)fread(readString,1,100,inputFile);
+    }
+    size = (int)ftell(inputFile);
+    fclose(inputFile);
+    fseek (outputFile, 0, SEEK_SET);
+    fprintf(outputFile,"Size of file is %d \n",size);
+    printf("Size of file is %d \n",size);
+    fprintf(outputFile, "number of the matches = %d \n", numbers);
+    printf("number of the matches = %d \n", numbers);
+    fclose(outputFile);
+    return 0;
+}
